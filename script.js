06dfc9e8bfa1c013b87cf9b5dd3c5cf7f6e9f911
uraned7 <83013743+uraned7@users.noreply.github.com>
2023-02-28 00:22:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -10,14 +10,21 @@ function showProduct(jason){
     let allProducts = jason.products;
     for (const product of allProducts) {
         content.innerHTML += `
-        <div class="ItemDiv">
-            <h1>${product.title}</h1>
-            <p>${product.category}</p>
-            <img src="${product.thumbnail}"></img>
+        <div class="ItemDiv" onclick="popup('${product.description}')">
+            <div class="ItemHeader">
+                <h1>${product.title}</h1>
+                <p id="Category">${product.category}</p>
+            </div>
+            <img id="ItemImage" src="${product.thumbnail}"></img>
             <p>${product.description}</p>
-            <p>${product.price} рублей</p>
-            <p>${product.rating} звёзд</p>
+            <div class="ItemInfo">
+                <p>${product.price} рублей</p>
+                <p id="Rating">${product.rating} звёзд</p>
+            </div>
+            <input id="Buy" type="button" value="Купить">
+
         </div>
+        
         `
     }
 }
@@ -26,3 +33,7 @@ let TB = document.querySelector("#TB").value;
 fetch('https://dummyjson.com/products/search?q={TB}')
 .then(res => res.json())
 .then(console.log);
+
+function popup(text){
+    alert(text)
+}
